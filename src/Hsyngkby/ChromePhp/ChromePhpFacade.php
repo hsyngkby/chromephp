4670bd931cedc4faa6e91f84b64dc7472cc1a9fc
Hüseyin GÖKBAY <huseyingokbay@gmail.com>
2014-03-04 17:27:56 +0200

    upload

---
+++ src/Hsyngkby/ChromePhp/ChromePhpFacade.php
@@ -0,0 +1,16 @@
+<?php namespace Hsyngkby\ChromePhp;
+
+
+use Illuminate\Support\Facades\Facade;
+
+class ChromePhpFacade extends \Hsyngkby\ChromePhp\ChromePhp {}
+/*
+class ChromePhpFacade extends Facade {
+
+	protected static function getFacadeAccessor()
+	{
+		return 'chromephp';
+	}
+
+}
+*/
---
+++ src/Hsyngkby/ChromePhp/ChromePhpServiceProvider.php
@@ -0,0 +1,74 @@
+<?php namespace Hsyngkby\ChromePhp;
+
+use Illuminate\Support\ServiceProvider;
+
+class ChromePhpServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		// Register the package
+		$this->package('hsyngkby/chrome-php', 'chromephp', __DIR__.'/../../');
+
+		// Register IoC bindings
+		$this->registerBindings();
+
+		// Shortcut so developers don't need to add an Alias in app/config/app.php
+		if ($alias = $this->app['config']->get('chromephp::alias', 'l'))
+		{
+			$this->app->booting(function() use ($alias)
+			{
+				$loader = \Illuminate\Foundation\AliasLoader::getInstance();
+
+				$loader->alias($alias, '\Hsyngkby\ChromePhp\ChromePhpFacade');
+			});
+		}
+
+		// Include various files
+		require __DIR__ . '/../../helpers.php';
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//include "ChromePhp.php";
+	}
+
+	/**
+	 * Register IoC bindings
+	 * @return void
+	 */
+	public function registerBindings()
+	{
+		$this->app->singleton('chromephp', function($app)
+		{
+			return new ChromePhp();
+		});
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
